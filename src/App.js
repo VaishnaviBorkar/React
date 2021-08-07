11a4a82db89480b1472e73ab866e18982e8fdaea
VaishnaviBorkar <borkarvaishnavi85@gmail.com>
2021-08-07 22:33:16 +0530

    css to button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Header from "./components/Header";
+//css for button 
+const buttonStyling={
+  backgroundColor:"red",
+  color:"white",
+  padding:50
+}
 function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>hellow welcome to react</h1>
-      <Header title="myHeading" id={1}/> 
-      <Header title="secondaryprop"/> 
+      {/* <h1>hellow welcome to react</h1> */}
+      {/* <Header title="myHeading" id={1}/>  */}
+      {/* <Header title="secondaryprop"/>  */}
+      {/* <Header/>  */}
       <Header/> 
-      <Header/> 
+      {/* inline css */}
+      <button className="btn btn-primary" style={{padding:"20px"}}> ADD </button>
+      <button style={buttonStyling}>Add</button>
+ 
       </div>
       </div>
   );
+ 
 }
+
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,14 @@ import  PropTypes from 'prop-types';
 // function base component
 const Header =(props)=>{
     return(
-        <div>
-            {/* <h1 className="header">TaskTracker </h1> */}
-            <h2 className="title">{props.title} {props.id}</h2>
-        </div>
+        // <div>
+        //     {/* <h1 className="header">TaskTracker </h1> */}
+        //     <h2 className="title">{props.title} {props.id}</h2>
+        // </div>
         //react fragment <> child elements </>
+        <h1>Task Tracker</h1>
     );
+   
 }
 Header.defaultProps={
     title:"default Props",
