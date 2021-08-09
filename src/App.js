b86300a1ce07ace3d44ad5da19c6c1f6220421ab
VaishnaviBorkar <borkarvaishnavi85@gmail.com>
2021-08-09 17:17:39 +0530

    state with function based components

--- src/App.js
+++ src/App.js
@@ -19,42 +19,91 @@
 // } */}
 //State and state management 
 // 1. using class
-import { Component} from 'react';
-import Person from "./components/Person"
+// import { Component} from 'react';
+// import Person from "./components/Person"
 
-class App extends Component{
-  state = {
-    person:[
-    {name:"joy",age:20},
-    {name:"git",age:29},
-    {name:"niya",age:21}
-  ],
-  otherState:"otherState"
-  }
-  changeState =()=>{
+// class App extends Component{
+//   state = {
+//     person:[
+//     {name:"joy",age:20},
+//     {name:"git",age:29},
+//     {name:"niya",age:21}
+//   ],
+//   otherState:"otherState"
+//   }
+//   changeState =()=>{
 // console.log("button clicked");
 // change name state
 //states are immutable (cantChange)
 //use setState to change state in virtual DOM it will not change the actual dom. 
 //setState only used in class bace component
-this.setState({
-  person:[
- {name:"vaishnavi",age:21},
-  {name:"Mona",age:20},
-  {name:"yogita",age:24}] 
-  })
+// this.setState({
+//   person:[
+//  {name:"vaishnavi",age:21},
+//   {name:"Mona",age:20},
+//   {name:"yogita",age:24}] 
+//   })
+//   console.log(this.state.person);
+  //values changed in viewport only not in actual Dom.
+  // }
+
+  // render(){
+  //   return(
+  //     <div>
+  //     <h2>class base component</h2>
+  //     <Person name="Ketki" age="45"/>
+  //     <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+  //     <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+  //     <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+  //  <button onClick={this.changeState}>Change state</button>
+  //     </div>
+  //   )
+  
+  // }
+
+// }
+// export default App;
+//function based states
+// we use useState hook to create function based component.it takes 2 argument as a 
+//array of the initial state and the set function(used to chane the value).
+//it does not edit the state.it deletes the state and create a ne state passed in setstate function.
+import React from 'react'
+import Person from './components/Person'
+import { useState } from 'react'
+const App = () => {
+  const [personstate,changepersonState]=useState(
+    {
+      person:[
+      {name:"vaish",age:20},
+      {name:"mona",age:21},
+      {name:"harita",age:10}
+
+    ],
+    otherState:"otherstate"
   }
-  render(){
-    return(
-      <div>
-      <h2>class base component</h2>
-      <Person name="Ketki" age="45"/>
-      <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-      <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
-      <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
-   <button onClick={this.changeState}>Change state</button>
-      </div>
-    )
+  )
+  console.log(personstate);
+
+  const changeState=()=>{
+    changepersonState({
+      person:[
+        {name:"vaishnavi",age:20},
+        {name:"MonaGupta",age:21},
+        {name:"HaritaShelake",age:10}
+  
+      ]
+
+    })
   }
+  return (
+    <div>
+      <Person name={personstate.person[0].name} age={personstate.person[0].age}/>
+      <Person name={personstate.person[1].name} age={personstate.person[1].age}/>
+      <Person name={personstate.person[2].name} age={personstate.person[2].age}/>
+      <button onClick={changeState}>Chane state with function</button>
+    </div>
+    
+  )
 }
-export default App;
+
+export default App
